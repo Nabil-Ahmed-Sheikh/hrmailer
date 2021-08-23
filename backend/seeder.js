80609e720998bca6fb583a6a7b3cc5827a39ce94
Nabil Ahmed <nabil.ahmednsu@gmail.com>
2021-08-24 01:05:43 +0600

    seeder added

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,9 +1,10 @@
-var mysql = require("mysql");
+import mysql from "mysql";
+
 var connection = mysql.createConnection({
   host: "localhost",
-  user: "dbuser",
-  password: "s3kreee7",
-  database: "my_db",
+  user: "root",
+  password: "",
+  database: "hrmailer",
 });
 
 connection.connect();
@@ -13,5 +14,34 @@ connection.query("SELECT 1 + 1 AS solution", function (err, rows, fields) {
 
   console.log("The solution is: ", rows[0].solution);
 });
+
+connection.query("DROP TABLE IF EXISTS users", function (err, rows, fields) {
+  if (err) throw err;
+  console.log("users table dropped");
+});
+
+connection.query(
+  "CREATE TABLE users (firstName varchar(255), lastName varchar(255), email varchar(255), PRIMARY KEY (email))",
+  function (err, rows, fields) {
+    if (err) throw err;
+    console.log("users table created");
+  }
+);
+
+connection.query(
+  `INSERT INTO users( firstName, lastName, email ) 
+  VALUES ('Zehad', 'Hassan', 'zehad@example,com'), 
+  ('Jane', 'Doe', 'jane@example.com'), 
+  ('Billy', 'Wales', 'billy@example.com'),
+  ('Miranda', 'Moose', 'moose@example.com'), 
+  ('John', 'Doe', 'john@example.com'), 
+  ('Zahid', 'Hassan', 'zahid@example.com'), 
+  ('Jalal', 'Uddin', 'jalal@example.com'), 
+  ('Jahangir', 'Pasha', 'pasha@example.com')`,
+  function (err, rows, fields) {
+    if (err) throw err;
+    console.log("Inserted dummy rows");
+  }
+);
 
 connection.end();
